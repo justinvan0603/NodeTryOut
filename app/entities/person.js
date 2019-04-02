c8266e3dccc781f51828aeb18e9fd658563fbe0c
JustinVan <jtvan13520776@gmail.com>
2019-04-02 15:26:56 +0200

    Delete

--- app/entities/person.js
+++
@@ -1,9 +0,0 @@
-var mongoose =require("mongoose");
-
-var personSchema = mongoose.Schema({
-    name : String,
-    address: String
-});
-var Person = mongoose.model("person",personSchema,"person");
-
-module.exports = Person;
